0cb1fe2b92bf877564d083e1ef093974a3ccdbe0
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-11-21 19:41:01 -0600

    finished 11

--- starter-code/app.js
+++ starter-code/app.js
@@ -184,17 +184,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scope();
+joe.scope();
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scopeArrow();
+joe.scopeArrow();
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// 'this' is the constructor and its properties that are being referred to when the scope proto is invoked
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// 'this' is the properties of the global object/window
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// 'this' is different when an arrow function is used because there isn't a scope being established so it refers to the global object.
